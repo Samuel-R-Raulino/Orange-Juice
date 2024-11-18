a85b9dd8050c1b7a0eac0c8c0876698ab4fc718a
Hendrill <etaleliton@gmail.com>
2024-11-18 09:16:15 -0300

    Add files via upload

--- cadastro.py
+++ cadastro.py
@@ -7,11 +7,12 @@ import sqlite3
 def clear():
     os.system('cls')
 
+
 clear()
 
 
 def conectar_bd():
-    conn = sqlite3.connect('usuarios.db') 
+    conn = sqlite3.connect('usuarios.db')
     return conn
 
 
@@ -24,6 +25,7 @@ def criar_tabela():
                         senha TEXT NOT NULL)''')
     conn.commit()
     conn.close()
+
 
 criar_tabela()
 
@@ -44,8 +46,7 @@ def cadastrar_usuario():
             cursor.execute("INSERT INTO usuarios (usuario, senha) VALUES (?, ?)", (usuario, senha))
             conn.commit()
             messagebox.showinfo("Sucesso", "Usuário cadastrado com sucesso!")
-            janela_cadastro.grid_forget() 
-            janela_login.grid(row=0, column=0) 
+            exibir_tela_login()
 
         conn.close()
     else:
@@ -71,46 +72,35 @@ def validar_login():
     else:
         messagebox.showwarning("Aviso", "Preencha todos os campos.")
 
-def exibir_interface_principal():
-  
-    janela_login.grid_forget()
 
- 
+def exibir_tela_login():
+    janela_cadastro.grid_forget()
+    janela_login.grid(row=0, column=0, columnspan=6, sticky="nsew")
+
+
+def exibir_tela_cadastro():
+    janela_login.grid_forget()
+    janela_cadastro.grid(row=0, column=0, columnspan=6, sticky="nsew")
+
+
+def exibir_interface_principal():
+    janela_login.grid_forget()
+    janela_cadastro.grid_forget()
+
     titulo = tk.Label(janela, text="Orange Juice Online Games", font=("Arial", 29, "bold"), bg="#C83436", fg="white", padx=5, pady=1)
     titulo.grid(row=1, column=0, columnspan=6, sticky="we")
 
     subtitulo = tk.Label(janela, text="Bem-vindo ao nosso sistema virtual de jogos online Orange Juice!", font=("Arial", 13, "bold"), bg="#C83436", fg="white", padx=5, pady=1)
     subtitulo.grid(row=2, column=0, columnspan=6, sticky="we")
 
- 
-    wid = 56
-    conta = tk.Button(janela, text="Conta", width=wid, height=2, command=lambda: print("conta"), font=("Arial", 10), bg="#FF914B", relief="solid", highlightcolor="white")
-    navegacao = tk.Button(janela, text="Navegação", width=wid, height=2, command=lambda: print("navegacao"), font=("Arial", 10), bg="#FF914B", relief="solid")
-    novidades = tk.Button(janela, text="Novidades", width=wid, height=2, command=lambda: print("novidades"), font=("Arial", 10), bg="#FF914B", relief="solid")
-    biblioteca = tk.Button(janela, text="Biblioteca", width=wid, height=2, command=lambda: print("biblioteca"), font=("Arial", 10), bg="#FF914B", relief="solid")
-
-    conta.grid(column=0, row=3, padx=10, pady=10)
-    navegacao.grid(column=1, row=3, padx=10, pady=10)
-    novidades.grid(column=2, row=3, padx=10, pady=10)
-    biblioteca.grid(column=3, row=3, padx=10, pady=10)
-
-    conteudo_frame = tk.Frame(janela, bg="white", bd=1, relief="solid")
-    conteudo_frame.grid(row=4, column=0, columnspan=6, sticky="NSEW", padx=10, pady=10)
-
-    conteudo = tk.Label(conteudo_frame, text="", font=("Arial", 10), bg="white", anchor="w", padx=10, pady=10)
-    conteudo.pack(fill="both", expand=True)
-
-    janela.grid_rowconfigure(4, weight=1)
-    janela.grid_columnconfigure(5, weight=1)
 
 janela = tk.Tk()
-janela.title("Orange Juice")    
+janela.title("Orange Juice")
 janela.geometry("800x600")
 janela.configure(bg="#C83436")
 
+# Tela de Login
 janela_login = tk.Frame(janela, bg="#C83436")
-janela_login.grid(row=0, column=0, columnspan=6, sticky="nsew", padx=10, pady=10)
-
 
 usuario_label_login = tk.Label(janela_login, text="Usuário:", font=("Arial", 14), fg="white", bg="#C83436")
 usuario_label_login.grid(row=0, column=0, padx=10, pady=10)
@@ -122,15 +112,14 @@ senha_label_login.grid(row=1, column=0, padx=10, pady=10)
 entrada_senha_login = tk.Entry(janela_login, font=("Arial", 14), width=20, show="*")
 entrada_senha_login.grid(row=1, column=1, padx=10, pady=10)
 
-
 botao_login = tk.Button(janela_login, text="Entrar", font=("Arial", 14), bg="#FF914B", command=validar_login)
 botao_login.grid(row=2, column=0, columnspan=2, pady=20)
 
-botao_cadastro = tk.Button(janela_login, text="Cadastre-se", font=("Arial", 14), bg="#FF914B", command=lambda: janela_cadastro.grid(row=0, column=0))
+botao_cadastro = tk.Button(janela_login, text="Cadastre-se", font=("Arial", 14), bg="#FF914B", command=exibir_tela_cadastro)
 botao_cadastro.grid(row=3, column=0, columnspan=2)
 
+# Tela de Cadastro
 janela_cadastro = tk.Frame(janela, bg="#C83436")
-janela_cadastro.grid(row=0, column=0, columnspan=6, sticky="nsew", padx=10, pady=10)
 
 usuario_label_cadastro = tk.Label(janela_cadastro, text="Usuário:", font=("Arial", 14), fg="white", bg="#C83436")
 usuario_label_cadastro.grid(row=0, column=0, padx=10, pady=10)
@@ -145,4 +134,8 @@ entrada_senha.grid(row=1, column=1, padx=10, pady=10)
 botao_cadastrar = tk.Button(janela_cadastro, text="Cadastrar", font=("Arial", 14), bg="#FF914B", command=cadastrar_usuario)
 botao_cadastrar.grid(row=2, column=0, columnspan=2, pady=20)
 
+botao_voltar = tk.Button(janela_cadastro, text="Voltar", font=("Arial", 14), bg="#FF914B", command=exibir_tela_login)
+botao_voltar.grid(row=3, column=0, columnspan=2)
+
+exibir_tela_login()
 janela.mainloop()
